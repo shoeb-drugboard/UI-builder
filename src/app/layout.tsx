3e7b303afb5b2c92395c009fa2e34c497c5b5e2e
shoeb-drugboard <frontend@drugboard.ai>
2025-01-02 17:58:29 +0530

    app setup: NextUI & Storybook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Providers } from "./providers";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -24,10 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from "tailwindcss";
-
+import { nextui } from "@nextui-org/theme";
+import tailwindAnimation from "tailwindcss-animate";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/stories/**/*.{js,ts,jsx,tsx,mdx}",
+    "./stories/**/*.{js,ts,jsx,tsx,mdx}",
+    "./.storybook/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -12,7 +17,11 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        "smooth-bounce": "bounce 1s 3 ease-in-out",
+      },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui(), tailwindAnimation],
 } satisfies Config;
