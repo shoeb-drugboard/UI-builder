d219a368e475071a943da535c54450c4cde7e76e
shoeb-drugboard <frontend@drugboard.ai>
2025-02-20 00:11:42 +0530

    refactor: simplify next.config.ts and update type definitions

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-const isProd = process.env.NODE_ENV === "production";
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? "/UI-bulider/" : "",
-  basePath: isProd ? "/UI-builder" : "",
-  output: "export",
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
